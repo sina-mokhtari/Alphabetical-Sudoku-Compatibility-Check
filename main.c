8d4a657772756b5bb7cbcf783f640dff6f53f986
sina-mokhtari <sinamokhtari2000@gmail.com>
2022-05-11 01:14:28 +0430

    adding methods removeExtrax, toLower, setDHW

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
@@ -7,16 +8,14 @@
 
 #define FIFO_FILE "/tmp/fifo_twoway"
 
-void reverse_string(char *);
+void removeExtras(char *s);
+void toLower(char *s);
+int setDHW(char *s, int *dimention, int *height, int *width);
 
 int main() {
     int parentPid = getpid(), firstChildPid = -10, secondChildPid = -10,
         thirdChildPid = -10, fourthChildPid = -10;
     int temp;
-
-    //-----------------------------
-
-    //------------------------------
 
     int pid = fork();
 
@@ -40,9 +39,28 @@ int main() {
     } else
         firstChildPid = getpid();
 
-    // char *args[15];
-    // temp = getpid();
+
+
     if (getpid() == parentPid) {
+        char *buffer = 0;
+        long length;
+        FILE *f = fopen("testcase1.txt", "rb");
+
+        if (f) {
+            fseek(f, 0, SEEK_END);
+            length = ftell(f);
+            fseek(f, 0, SEEK_SET);
+            buffer = malloc(length);
+            if (buffer) {
+                fread(buffer, 1, length, f);
+            }
+            fclose(f);
+        }
+        removeExtras(buffer);
+        toLower(buffer);
+        int dimention = buffer[0], height = buffer[2], width = buffer[4];
+        setDHW(buffer, &dimention, &height, &width);
+
         int fd;
 
         // FIFO file path
@@ -50,21 +68,20 @@ int main() {
 
         mkfifo(myfifo, 0666);
 
-        char arr1[80], arr2[80]="yeay";
-        //while (1) {
-            // Open FIFO for write only
-            fd = open(myfifo, O_WRONLY);
+        char arr1[1000], arr2[80] = "yeay";
 
-            write(fd, arr2, strlen(arr2) + 1);
-            close(fd);
+        fd = open(myfifo, O_WRONLY);
 
-            fd = open(myfifo, O_RDONLY);
+        write(fd, buffer, strlen(buffer) + 1);
+        close(fd);
 
-            read(fd, arr1, sizeof(arr1));
+        fd = open(myfifo, O_RDONLY);
 
-            printf("User2: %s\n", arr1);
-            close(fd);
-       // }
+        read(fd, arr1, sizeof(arr1));
+
+        printf("User2: %s\n", arr1);
+        close(fd);
+
     }
 
     else if (getpid() == firstChildPid) {
@@ -79,24 +96,24 @@ int main() {
         // mkfifo(<pathname>,<permission>)
         mkfifo(myfifo, 0666);
 
-        char str1[80], str2[80], str3[80] = "got it : ";
-       // while (1) {
-            // First open in read only and read
-            fd1 = open(myfifo, O_RDONLY);
-            read(fd1, str1, 80);
+        char str1[1000], str2[1000], str3[1000] = "got it : \n";
+        // while (1) {
+        // First open in read only and read
+        fd1 = open(myfifo, O_RDONLY);
+        read(fd1, str1, 1000);
 
-            // Print the read string and close
-           // printf("User1: %s\n", str1);
-            close(fd1);
-            strcat(str3, str1);
-            strcpy(str2, str3);
-            // Now open in write mode and write
-            // string taken from user.
-            fd1 = open(myfifo, O_WRONLY);
-            //fgets(str2, 80, stdin);
-            write(fd1, str2, strlen(str2) + 1);
-            close(fd1);
-       // }
+        // Print the read string and close
+        // printf("User1: %s\n", str1);
+        close(fd1);
+        strcat(str3, str1);
+        strcpy(str2, str3);
+        // Now open in write mode and write
+        // string taken from user.
+        fd1 = open(myfifo, O_WRONLY);
+        // fgets(str2, 80, stdin);
+        write(fd1, str2, strlen(str2) + 1);
+        close(fd1);
+        // }
     } else if (getpid() == secondChildPid) {
         char *args[] = {"./secondChild", NULL};
         execv(args[0], args);
@@ -112,54 +129,51 @@ int main() {
     return 0;
 }
 
-void mainProcess() {
-    int fd;
-    char readbuf[80];
-    char end[10];
-    int to_end;
-    int read_bytes;
+void removeExtras(char *s) {
+    char *d = s;
+    do
+        while (*d == ' ' || *d == '\t' || *d == '\r' || *d == '\n') ++d;
 
-    /* Create the FIFO if it does not exist */
-    mkfifo(FIFO_FILE, S_IFIFO | 0640);
-    strcpy(end, "end");
-    fd = open(FIFO_FILE, O_RDWR);
-    // while (1) {
-    /*read_bytes = read(fd, readbuf, sizeof(readbuf));
-    readbuf[read_bytes] = '\0';
-    printf("FIFOSERVER: Received string: \"%s\" and length is %d\n",
-           readbuf, (int)strlen(readbuf));
-    to_end = strcmp(readbuf, end);
-
-    if (to_end == 0) {
-        close(fd);
-        break;
-    }
-    reverse_string(readbuf);
-    printf("FIFOSERVER: Sending Reversed String: \"%s\" and length is %d\n",
-           readbuf, (int)strlen(readbuf));*/
-    write(fd, readbuf, strlen(readbuf));
-    /*
-    sleep - This is to make sure other process reads this, otherwise this
-    process would retrieve the message
-    */
-    sleep(2);
-    //}
-    return;
+    while (*s++ = *d++);
 }
 
-void reverse_string(char *str) {
-    int last, limit, first;
-    char temp;
-    last = strlen(str) - 1;
-    limit = last / 2;
-    first = 0;
-
-    while (first < last) {
-        temp = str[first];
-        str[first] = str[last];
-        str[last] = temp;
-        first++;
-        last--;
+void toLower(char *s) {
+    while (*s) {
+        if (*s > 64 && *s < 91) *s += 32;
+        s++;
     }
-    return;
+}
+
+int setDHW(char *s, int *dimention, int *height, int *width) {
+    int i = 0, j;
+    char temp[10];
+    while (*s) {
+        if (*(s + i) == '*') break;
+        i++;
+    }
+
+    strncpy(temp, s, i);
+    *dimention = atoi(temp);
+
+    j = i + 1;
+    while (*s) {
+        if (*(s + j) == '*') break;
+        j++;
+    }
+
+    strncpy(temp, s + i + 1, j - i - 1);
+    *height = atoi(temp);
+
+    i = j + 1;
+
+    while (*s) {
+        if (*(s + i) < 48 || *(s + i) > 57) break;
+        i++;
+    }
+
+    strncpy(temp, s + j + 1, i - j - 1);
+    *width = atoi(temp);
+
+    // first row index in buffer
+    return i;
 }
